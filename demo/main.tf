7781231b00b3ba8d53db0dc261f2c121ad917e51
Philippe Vienne <philippe@skyloud.fr>
2024-07-10 18:03:56 +0200

    Add files

---
+++ demo/main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "skyloud-sparks-training"
+
+    workspaces {
+      name = "azure-demo"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
